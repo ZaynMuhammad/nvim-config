ac45d73f0a3e59d59122a0c7174204d771f974d4
zaynmuhammad <zaynmuhammad95@gmail.com>
2026-06-06 17:19:23 -0400

    Set explorer to false

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
